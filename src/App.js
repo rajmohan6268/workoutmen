ba67c95f4dc1d0714d77fd34e1d2ac33ab6b1ff2
rajmohan6268 <rajmohan6268@gmail.com>
2021-07-23 19:40:22 +0530

    init starter with react and tailwind

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-blue-400 h-screen grid">
+      <div className=" m-auto ">
+        <h1 className="text-5xl text-center font-bold  text-white">
+          Tailwindcss
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
